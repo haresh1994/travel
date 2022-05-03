cee8ad360a91ca4da0f8212c8efe145fc1565ca0
unknown <hareshhariharan1994@gmail.com>
2022-05-03 13:45:51 +0530

    add

---
+++ travelapp/models.py
@@ -0,0 +1,21 @@
+
+from django.shortcuts import render
+from django.db import models
+# Create your models here.
+class Place(models.Model):
+    objects = None
+    name=models.CharField(max_length=250)
+    img=models.ImageField(upload_to='pics')
+    desc=models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Team(models.Model):
+
+        name = models.CharField(max_length=250)
+        img= models.ImageField(upload_to='pics')
+        desc = models.TextField()
+
+
+
